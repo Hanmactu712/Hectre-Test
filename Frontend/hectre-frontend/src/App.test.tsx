0e5812e10c777da40eefefe44d714795c728fcd8
Dang Dinh Duc <ducdd.it@gmail.com>
2022-02-10 11:36:19 +0700

    update test

--- Frontend/hectre-frontend/src/App.test.tsx
+++ Frontend/hectre-frontend/src/App.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import React from "react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("render main chemical page", () => {
   render(<App />);
   const chemicalElements = screen.getAllByText(/CHEMICALS/i);
   expect(chemicalElements.length).toBeGreaterThan(0);
